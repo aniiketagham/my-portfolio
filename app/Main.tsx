f03c551962555c47f6afa1a8c53a642bcb7abc81
AniketAgham <aniketagham1509@gmail.com>
2025-02-01 23:31:56 +0530

    main updated

--- app/Main.tsx
+++ app/Main.tsx
@@ -25,10 +25,7 @@ export default function Home({ posts }) {
             Hi, I’m Aniket Agham
           </h1>
           <h2 className="prose text-lg text-gray-600 dark:text-gray-400">
-            {`Welcome to my blog - ${siteMetadata.description}. I am the co-founder of Cylynx, a data
-            scientist by profession and economist by training. In my free time, I like developing side projects and `}
-            <Link href="/blog">blogging</Link>
-            {' about them. Have a good read!'}
+            {`A passionate Data Scientist and Analytics Professional with a deep love for uncovering insights and solving complex problems through data. With 1+ year of experience in leveraging data to drive impactful business decisions, I specialize in Analytics, Machine Learning, NLP, and BI tools.`}
           </h2>
           <TypingEffect />
           <div className="my-8">
@@ -59,6 +56,95 @@ export default function Home({ posts }) {
         <MDXLayoutRenderer code={author.body.code} />
       </AuthorLayout>
 
+{/* Education Section */}
+<div id="education" className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Education
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Here's a look at my academic background.
+          </p>
+        </div>
+        <div className="container py-12">
+          <div className="space-y-8">
+            {/* Education Item 1 */}
+            <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md">
+              <h3 className="text-xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
+                Bachelor of Engineering - Computer Engineering
+              </h3>
+              <p className="mt-2 text-lg text-gray-600 dark:text-gray-400">
+                St. Vincent Pallotti College of Engineering and Technology
+              </p>
+              <p className="mt-2 text-md text-gray-600 dark:text-gray-400">
+                Coursework: Java, Python, Data Structures and Algorithms, DBMS, Neural Networks
+              </p>
+            </div>
+            {/* Education Item 2 */}
+            <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md">
+              <h3 className="text-xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
+                Diploma in Data Science
+              </h3>
+              <p className="mt-2 text-lg text-gray-600 dark:text-gray-400">
+                Indian Institute of Technology, Madras
+              </p>
+              <p className="mt-2 text-md text-gray-600 dark:text-gray-400">
+                Coursework: Mathematics, Python, R, Statistics, Machine Learning, Deep Learning, NLP, Business Analytics
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Experience Section */}
+      <div id="experience" className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Experience
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Here's an overview of my professional experience.
+          </p>
+        </div>
+        <div className="container py-12">
+          <div className="space-y-8">
+            {/* Experience Item */}
+            <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md">
+              <h3 className="text-xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
+                Bussiness Analyst Apperentice - (2024 - 24)
+              </h3>
+              <p className="mt-2 text-lg text-gray-600 dark:text-gray-400">
+                Bizgrow Technology (OPC pvt. ltd)
+              </p>
+              <ul className="mt-2 text-md text-gray-600 dark:text-gray-400 list-inside list-disc pl-4">
+                <li>Analyzed major products, that drive revenue growth and prepared MIS reports for senior management's informed decision-making.
+                </li>
+                <li>Provided regular status updates to senior leadership through concise and clear presentations, ensuring project completion within a 10% faster timeline than planned.
+                </li>
+                <li>Collaborated with cross-functional stakeholders to gather and document business requirements, ensuring transparent communication.</li>
+              </ul>
+            </div>
+            <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md">
+              <h3 className="text-xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
+                Data Analyst Traniee - (2023 - 23)
+              </h3>
+              <p className="mt-2 text-lg text-gray-600 dark:text-gray-400">
+                AliGn InfoTech
+              </p>
+              <ul className="mt-2 text-md text-gray-600 dark:text-gray-400 list-inside list-disc pl-4">
+                <li>Utilized SQL and Python for data extraction, cleaning, and exploratory data analysis (EDA) on large datasets.
+                </li>
+                <li>Conducted statistical analysis to uncover trends, patterns, and insights, enhancing data-driven decision-making.
+                </li>
+                <li>Created and maintained dashboards and data reports using 15+ Tableau, translating complex data into clear, actionable insights for senior leadership.
+                </li>
+                <li>Performed advanced Excel functions (VLOOKUP, Pivot Tables, Micros) for in-depth analysis, report generation, and automation of data processes, saving 7+ hours weekly.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/* Skills Section */}
       <div id="skills" className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
@@ -77,25 +163,27 @@ export default function Home({ posts }) {
                 Tools
               </h2>
               <ul className="mt-4 list-inside list-disc pl-4 text-lg text-gray-600 dark:text-gray-400">
-                <li>VS Code</li>
-                <li>Git & GitHub</li>
-                <li>JIRA</li>
-                <li>Figma</li>
-                <li>Postman</li>
+                <li>Python, R</li>
+                <li>SQL</li>
+                <li>Power BI, Tableau</li>
+                <li>Excel</li>
+                <li>Git/Github</li>
+                <li>AWS,Azure,GCP </li>
               </ul>
             </div>
 
             {/* Technologies Column */}
             <div>
               <h2 className="text-xl  font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
-                Technologies
+                Techniques
               </h2>
               <ul className="mt-4 list-inside list-disc pl-4 text-lg text-gray-600 dark:text-gray-400">
-                <li>JavaScript & TypeScript</li>
-                <li>React.js & Next.js</li>
-                <li>Node.js</li>
-                <li>Tailwind CSS</li>
-                <li>SQL & NoSQL Databases</li>
+                <li>Analytics</li>
+                <li>Machine Learning</li>
+                <li>Visualization</li>
+                <li>Forecasting & Time Series</li>
+                <li>NLP</li>
+                <li>Statistics</li>
               </ul>
             </div>
 
@@ -105,10 +193,11 @@ export default function Home({ posts }) {
                 Soft Skills
               </h2>
               <ul className="mt-4 list-inside list-disc pl-4 text-lg text-gray-600 dark:text-gray-400">
-                <li>Problem Solving</li>
                 <li>Communication</li>
-                <li>Team Collaboration</li>
-                <li>Time Management</li>
+                <li>Team Work</li>
+                <li>Problem Solving</li>
+                <li>Adaptibility</li>
+                <li>Decision Making</li>
                 <li>Critical Thinking</li>
               </ul>
             </div>
